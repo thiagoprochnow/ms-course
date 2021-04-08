6b31ac3554a62981ad3c239174b47d15de8a4379
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-04-07 21:32:47 -0300

    Zuul authorization

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.hreurekaserver;
+package com.devsuperior.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
